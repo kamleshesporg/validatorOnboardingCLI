02b61b306c8f765383f0621451a5e7cba36d68de
kamleshesporg <kamlesh@espsofttech.org>
2025-05-27 12:38:30 +0530

    install-mrmintchain

--- install-mrmintchain.sh
+++ install-mrmintchain.sh
@@ -3,20 +3,23 @@
 
 echo "Mrmintchain Binary files downloading..."
 
-#mrmintchain binary fetching...
+echo "1. mrmintchain binary fetching..."
 curl -sSLO https://raw.githubusercontent.com/kamleshesporg/validatorOnboardingCLI/main/mrmintchain
-#mrmintd binary fetching...
+
+echo "2. mrmintd binary fetching..."
 curl -sSLO https://raw.githubusercontent.com/kamleshesporg/validatorOnboardingCLI/main/chain/ethermintd
 
-CONTAINER_CLI=docker
+echo -e "\xE2\x9C\x94 Binary files downloaded!"
 
+
+CONTAINER_CLI=docker
 image_tag=latest
  
 
 echo "===> Pulling mrmintchain Image"
-local image_name="docker.io/kamleshesp/mrmintchain:${image_tag}"
+image_name="docker.io/kamleshesp/mrmintchain:${image_tag}"
 
 echo "====>  ${image_name}"
 ${CONTAINER_CLI} pull "${image_name}"
 
-
+echo -e "\xE2\x9C\x94 Congratulations mrmintchain installed!"
